7597ced21fccdc0335c57eef77f34e6f9a53e5cc
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2024-06-18 21:27:21 +0200

    change Distance => Cosine

--- gradio/app/src/chatbots/parsers/templates/versioning.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-
-class Versioning(abc.ABC):
-    def __init__(self, parsers_list: list, index_main):
-        self.parsers_list = parsers_list
-        self.index_main = index_main
-        self.main_parser = parsers_list[index_main]
